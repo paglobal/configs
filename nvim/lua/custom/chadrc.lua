a2566f3080f02f372ac56bf68f8e734656a9c9bd
paglobal <jackamoah.pa@gmail.com>
2023-10-29 23:54:16 +0000

    feat: change vim keybindings for normal text deletion behaviour

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -17,27 +17,11 @@ vim.cmd([[
 vim.cmd([[vnoremap J :m '>+1<cr>gv=gv]])
 vim.cmd([[vnoremap K :m '<-2<cr>gv=gv]])
 
--- d, D, c, and C now delete text into the "1" register instead of the default register
--- always explicity yank text before you delete it with these characters to simulate the default behaviour
--- also, use x to simulate "cut" behaviour for arbitrarily selected text from now on
-vim.cmd([[nnoremap d "1d]])
-vim.cmd([[vnoremap d "1d]])
-vim.cmd([[nnoremap D "1D]])
-vim.cmd([[vnoremap D "1D]])
-vim.cmd([[nnoremap c "1c]])
-vim.cmd([[vnoremap c "1c]])
-vim.cmd([[nnoremap C "1C]])
-vim.cmd([[vnoremap C "1C]])
-
 -- global search and replace
 vim.cmd([[nnoremap <leader>gs :%s///gc]])
 
 -- "sane" behaviour for pasting in visual mode
 vim.cmd([[xnoremap p P]])
-
--- use P paste from the "1" register in case you want to paste something that was most recently deleted with d, D, c or C
-vim.cmd([[nnoremap P "1p]])
-vim.cmd([[xnoremap P "1P]])
 --
 -- !!! KEYBINDINGS END !!!
 --
@@ -47,8 +31,8 @@ vim.cmd([[xnoremap P "1P]])
 local M = {}
 
 M.ui = {
-	theme = "ayu_dark",
-	theme_toggle = { "gruvbox", "gruvchad" },
+	theme = "gruvchad",
+	theme_toggle = { "gruvchad", "gruvchad" },
 	telescope = { style = "bordered" },
 	statusline = {
 		theme = "minimal", -- default/vscode/vscode_colored/minimal
